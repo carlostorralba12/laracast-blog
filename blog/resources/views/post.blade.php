66c914211668752b692ffe9d53b930e42c0e46ee
carlostorralba12 <carlostorralba12@gmail.com>
2021-10-05 11:39:06 +0200

    Add project laracat with Section 2: Episode 10

---
+++ blog/resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<title>My Blog</title>
+<link rel="stylesheet" href="/css/app.css">
+<body>
+    <article>
+
+        <p>
+            <?= $post; ?>
+        </p>
+    </article>
+
+    <a href="/">Go back</a>
+</body>
---
+++ blog/routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use \App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts');
+});
+
+Route::get('posts/{post}', function ($slug) {
+
+    // Find a post its slug and pass it to a view called "post"
+
+    $post = Post::find($slug);
+    return view('post', [
+        'post' => $post
+    ]);
+    /*$path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    if(! file_exists($path)){
+        //dd('file does not exists');
+        //ddd('file does not exists');
+        //abort(404);
+        return redirect('/');
+    }
+
+    //$post = file_get_contents($path);
+    //now()->addMinute()
+    $post = cache()->remember("posts.{$slug}", 1200, fn() =>  file_get_contents($path));
+    return view('post', [
+        'post' =>  $post//$post
+
+    ]);*/
+})->where('post', '[A-z_\-]+');
+
+//Another methods to constraints
+/*whereAlpha();
+whereAlphaNumeric();
+whereNumber();*/
+
