19ae7813caa695460893e35544ac1c50ebe05026
carlostorralba12 <carlostorralba12@gmail.com>
2021-10-06 13:56:22 +0200

    Add episode 28

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -16,42 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        User::factory()->create([
+            'name' => 'Alfredo'
         ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My family post',
-            'slug' => 'my-family-post',
-            'excerpt'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'body' => '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tincidunt a dui quis sodales. Ut aliquet nunc ex, id pellentesque tellus laoreet vitae. Phasellus sit amet bibendum lorem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque nisi tellus, ornare quis rutrum non, venenatis quis eros. In lorem libero, malesuada eget dignissim tristique, posuere sed lorem. Nunc varius placerat ipsum, sit amet iaculis libero dictum eget. Maecenas molestie dapibus lobortis. Aliquam sed quam accumsan dui imperdiet laoreet vel fringilla elit. Morbi tellus est, aliquam vitae condimentum et, aliquet tempus massa. Aenean non tincidunt lacus. Mauris vel nisi lacus. </p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My work post',
-            'slug' => 'my-work-post',
-            'excerpt'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'body' => '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tincidunt a dui quis sodales. Ut aliquet nunc ex, id pellentesque tellus laoreet vitae. Phasellus sit amet bibendum lorem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque nisi tellus, ornare quis rutrum non, venenatis quis eros. In lorem libero, malesuada eget dignissim tristique, posuere sed lorem. Nunc varius placerat ipsum, sit amet iaculis libero dictum eget. Maecenas molestie dapibus lobortis. Aliquam sed quam accumsan dui imperdiet laoreet vel fringilla elit. Morbi tellus est, aliquam vitae condimentum et, aliquet tempus massa. Aenean non tincidunt lacus. Mauris vel nisi lacus. </p>'
-        ]);
+        Post::factory()->create();
     }
 }
