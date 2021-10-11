33bf75fac9ec05f557e1a6bdb5e16e9c34d4f740
carlostorralba12 <carlostorralba12@gmail.com>
2021-10-11 09:11:18 +0200

    Episode 32 isn't finish, problem with merging attributes of class in blade components

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -1,23 +1,16 @@
 <x-layout>
-    @foreach ($posts as $post)
-        <article>
+    @include('_post-header')
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if($posts->count())
 
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
+            <x-posts-grid :posts="$posts"></x-posts-grid>
 
-            </h1>
+        @else
+            <p class="text-center">No posts yet. Please check back later</p>
+        @endif
 
-            <p>
-                By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
-            </p>
+    </main>
 
-            <div>
-                {{ $post->excerpt }}
-            </div>
-        </article>
-    @endforeach
 </x-layout>
 
 
