07fd1f0ccb9914cff65fa07b384b5724e44919a6
carlostorralba12 <carlostorralba12@gmail.com>
2021-10-18 14:14:54 +0200

    Episode 66

--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -1,37 +1,30 @@
 <x-layout>
-    <section class="py-8 max-w-md mx-auto">
-        <h1 class="text-lg font-bold mb-4">
-            Publish New Post
-        </h1>
+    <x-setting heading="Publish New Post">
+        <form method="POST" action="/admin/posts" enctype="multipart/form-data">
+            @csrf
+            <x-form.input name="title"></x-form.input>
+            <x-form.input name="slug"></x-form.input>
+            <x-form.input name="thumbnail" type="file"></x-form.input>
+            <x-form.textarea name="excerpt"></x-form.textarea>
+            <x-form.textarea name="body"></x-form.textarea>
 
-        <x-panel>
-            <form method="POST" action="/admin/posts" enctype="multipart/form-data">
-                @csrf
+            <x-form.field>
+                <x-form.label name="category"></x-form.label>
 
-                <x-form.input name="title"></x-form.input>
-                <x-form.input name="slug"></x-form.input>
-                <x-form.input name="thumbnail" type="file"></x-form.input>
-                <x-form.textarea name="excerpt"></x-form.textarea>
-                <x-form.textarea name="body"></x-form.textarea>
+                <select name="category_id" id="category_id">
+                    @foreach (\App\Models\Category::all() as $category)
+                        <option
+                            value="{{ $category->id }}"
+                            {{ old('category_id') == $category->id ? 'selected' : '' }}
+                        >{{ ucwords($category->name) }}</option>
+                    @endforeach
+                </select>
 
-                <x-form.field>
-                    <x-form.label name="category"></x-form.label>
-
-                    <select name="category_id" id="category_id">
-                        @foreach (\App\Models\Category::all() as $category)
-                            <option
-                                value="{{ $category->id }}"
-                                {{ old('category_id') == $category->id ? 'selected' : '' }}
-                            >{{ ucwords($category->name) }}</option>
-                        @endforeach
-                    </select>
-
-                    <x-form.error name="category"></x-form.error>
-                </x-form.field>
+                <x-form.error name="category"></x-form.error>
+            </x-form.field>
 
 
-                <x-form.button>Publish</x-form.button>
-            </form>
-        </x-panel>
-    </section>
+            <x-form.button>Publish</x-form.button>
+        </form>
+    </x-setting>
 </x-layout>
